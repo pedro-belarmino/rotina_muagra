f602816c76f2e4b9c7b2386a549e61fe3e2abae5
pedro-5by5 <pedro.gbelarmino@gmail.com>
2025-10-28 09:57:29 -0300

    criacao de arquivos para configuracao do muagrometro global

---
+++ src/components/GlobalCounter/Counter.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+
+export default function Counter() {
+    return (
+        <Container maxWidth="sm" sx={{ mt: 3, }}>
+
+            <img src="public/Muagrômetro Global Preto Sem número.png" width={100} alt="" />
+
+
+        </Container>
+    )
+}
